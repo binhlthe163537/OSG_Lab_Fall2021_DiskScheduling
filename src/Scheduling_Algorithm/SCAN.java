91a93a9dca3b83964f6fe259b605feee49248683
VietPT3502 <91371949+VietPT3502@users.noreply.github.com>
2021-10-25 14:24:38 +0700

    Sua file Scan

--- src/Scheduling_Algorithm/SCAN.java
+++ src/Scheduling_Algorithm/SCAN.java
@@ -14,54 +14,43 @@ public class Scan {
 
     public static void main() {
         Scanner input = new Scanner(System.in);
-        int sum = 0;
         int head;
         int[] queue;
-        int max;
+        int seekTime;
+        double avg;
         int dloc = 0;  //location of disk(head) in arr
 
-        System.out.println("Input no of disk locations");
+        System.out.print("Input no of disk locations: ");
         int qSize = Integer.parseInt(input.nextLine());
 
         queue = new int[qSize + 1];
-        System.out.println("Enter head position");
+        System.out.print("Enter head position: ");
         head = Integer.parseInt(input.nextLine());
 
-        System.out.println("Input elements in disk queue");
+        System.out.println("Input elements in disk queue: ");
         for (int i = 0; i < qSize; i++) {
             queue[i] = Integer.parseInt(input.nextLine());
         }
-
         queue[qSize] = head;
-        qSize++;
-
         //sort disk locations queue
         Arrays.sort(queue);
+        dloc = Arrays.binarySearch(queue, head);
 
-        max = queue[qSize - 1];
-
-        for (int i = 0; i < qSize; i++) {
-            if (head == queue[i]) {
-                dloc = i;
-                break;
-            }
+        for(int i = dloc; i < queue.length - 1; i++)
+        {
+            System.out.println("Move " + queue[i] + " to " + queue[i + 1]);
         }
 
-        for (int i = dloc; i >= 0; i--) {
-            System.out.print(queue[i] + "-->");
+        System.out.println("Move " + queue[queue.length - 1] + " to " + queue[dloc - 1]);
+
+        for(int i = dloc - 1; i > 0; i--)
+        {
+            System.out.println("Move " + queue[i] + " to " + queue[i - 1]);
         }
-
-        System.out.print("0-->");
-
-        for (int i = dloc + 1; i < qSize; i++) {
-            System.out.print(queue[i] + "-->");
-        }
-
-        System.out.println();
-
-        sum = head + max;
-        System.out.println(" Movement of total cylinders " + sum);
-
+        seekTime = 2 * queue[queue.length - 1] - queue[dloc] - queue[0];
+        avg = seekTime / (double)qSize;  
+        System.out.println("Total seek time is " + seekTime);
+        System.out.println("Average seek time is " + avg);
+        input.close();
     }
-
 }
